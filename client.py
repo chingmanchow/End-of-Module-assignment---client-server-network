a48adda2e22f262121c32487d1c4fa8efaeb8335
Christine Parry <christine.parry9@gmail.com>
2023-09-26 11:13:18 +0200

    txt file encoding, exeption handling

--- client.py
+++ client.py
@@ -1,12 +1,90 @@
+# import socket
+# import pickle
+# import os
+# import base64
+
+
+# def start_client():
+#     # Initialize socket
+#     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#     # Try to establish a connection
+#     try:
+#         client_socket.connect(('localhost', 12345))
+#     except socket.error as e:
+#         print(f"Error occurred while connecting: {e}")
+#         return
+
+#     # Create and populate a dictionary
+#     data_dict = {'name': 'Christine', 'age': 35, 'city': 'London'}
+
+#     # Serialize the dictionary
+#     serialized_data = pickle.dumps(data_dict)
+
+#     # Try to send the serialized dictionary to the server
+#     try:
+#         client_socket.sendall(serialized_data)
+#     except socket.error as e:
+#         print(f"Error occurred while sending data: {e}")
+#         return
+
+#     print("Dictionary has been sent")
+
+#     # Prepare the text file
+#     file_name = 'sample_file.txt'
+
+#     if not os.path.isfile(file_name):
+#         print(f"File {file_name} does not exist.")
+#         return
+
+#     with open(file_name, 'rb') as f:
+#         file_data = f.read()
+
+#     # Encode the file data
+#     encoded_file_data = base64.b64encode(file_data)
+
+#     # Get the size of the encoded file data
+#     size = len(encoded_file_data)
+
+#     # Try to transmit file name and size to the receiver
+#     try:
+#         client_socket.send(pickle.dumps({'file_name': file_name, 'file_size': size}))
+#     except socket.error as e:
+#         print(f"Error occurred while sending file metadata: {e}")
+#         return
+
+#     # Try to send the encoded file data
+#     try:
+#         client_socket.sendall(encoded_file_data)
+#     except socket.error as e:
+#         print(f"Error occurred while sending file data: {e}")
+#         return
+
+#     print("File has been sent.")
+
+#     client_socket.close()
+
+
+# if __name__ == "__main__":
+#     start_client()
+
 import socket
 import pickle
 import os
+import base64
+import time
 
 
 def start_client():
     # Initialize socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('localhost', 12345))
+
+    # Try to establish a connection
+    try:
+        client_socket.connect(('localhost', 12345))
+    except socket.error as e:
+        print(f"Error occurred while connecting: {e}")
+        return
 
     # Create and populate a dictionary
     data_dict = {'name': 'Christine', 'age': 35, 'city': 'London'}
@@ -14,43 +92,65 @@ def start_client():
     # Serialize the dictionary
     serialized_data = pickle.dumps(data_dict)
 
-    # Send the serialized dictionary to the server
-    client_socket.sendall(serialized_data)
+    # Try to send the serialized dictionary to the server
+    max_retries = 5
+    for i in range(max_retries):
+        try:
+            client_socket.sendall(serialized_data)
+            print("Dictionary has been sent")
+            break
+        except socket.error as e:
+            if i < max_retries - 1:  # i is zero indexed
+                time.sleep(1)  # wait a bit before trying again
+                continue
+            else:
+                print(f"Error occurred while sending data: {e}")
+                return
 
-    print("Dictionary has been sent")
+    # Prepare the text file
+    file_name = 'sample_file.txt'
+    if not os.path.isfile(file_name):
+        print(f"File {file_name} does not exist.")
+        return
 
+    with open(file_name, 'rb') as f:
+        file_data = f.read()
 
+    # Encode the file data
+    encoded_file_data = base64.b64encode(file_data)
 
-    #get teh txt file to send
-    file = open('sample_file.txt', 'rb')
+    # Get the size of the encoded file data
+    size = len(encoded_file_data)
 
-    #get the sze of the txt file
-    size = os.path.getsize('sample_file.txt')
+    # Try to transmit file name and size to the receiver
+    for i in range(max_retries):
+        try:
+            client_socket.send(pickle.dumps({'file_name': file_name, 'file_size': size}))
+            break
+        except socket.error as e:
+            if i < max_retries - 1:  # i is zero indexed
+                time.sleep(1)  # wait a bit before trying again
+                continue
+            else:
+                print(f"Error occurred while sending file metadata: {e}")
+                return
 
-    #transmit file name and size to the receiver
-    # client_socket.send("received_file.txt".encode())
-    # client_socket.send(str(size).encode())
-
-    #send all data to receiver
-    data = file.read()
-    client_socket.sendall(data)
-
-    print("File has been sent.")
+    # Try to send the encoded file data
+    for i in range(max_retries):
+        try:
+            client_socket.sendall(encoded_file_data)
+            print("File has been sent.")
+            break
+        except socket.error as e:
+            if i < max_retries - 1:  # i is zero indexed
+                time.sleep(1)  # wait a bit before trying again
+                continue
+            else:
+                print(f"Error occurred while sending file data: {e}")
+                return
 
     client_socket.close()
 
-        # Create and populate a text file
-    # with open('sample_file.txt', 'w') as f:
-    #     f.write('Hello, this is a sample file content.')
-
-
-    # Read and send the text file
-    # with open('sample_file.txt', 'rb') as f:
-    #     while True:
-    #         file_data = f.read(1024)
-    #         if not file_data:
-    #             break
-    #         client_socket.sendall(file_data)
 
 if __name__ == "__main__":
     start_client()
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import socket
 import pickle
-
+import base64
 
 def start_server():
     # Initialize socket
@@ -18,12 +18,40 @@ def start_server():
     received_dict = pickle.loads(data)
     print(f"Received dictionary: {received_dict}")
 
-    #print contents of txt file
-    file_data = conn.recv(1024).decode()
-    print("file data: " + file_data)
+    metadata =conn.recv(1024)
+    decoded_metadata = pickle.loads(metadata)
+    file_name = decoded_metadata['file_name']
+    file_size = decoded_metadata['file_size']
+
+    print(f"Receiving file: {file_name}")
+    print(f"Expected file size: {file_size}")
+
+    # Receiving the actual file data
+    received_data = b""
+    while len(received_data) < file_size:
+        data = conn.recv(4096)  # Receive data in chunks
+        if not data:
+            # Connection might have been closed
+            print("Connection closed before receiving complete data.")
+            return
+
+        received_data += data
+
+    # Decoding the file data from base64
+    file_data = base64.b64decode(received_data)
+
+    # Save the file
+    with open(file_name, 'wb') as file:
+        file.write(file_data)
+
+    print("File has been received and saved.")
+
+    # #print contents of txt file
+    # file_data = conn.recv(1024).decode()
+    # print("file data: " + file_data)
 
 
-    # Open text file for reading the received content - server only needs to know that it is a txt file
+    # # Open text file for reading the received content - server only needs to know that it is a txt file
     # with open('received_file.txt', 'rb') as f:
     #     content = f.read()
     #     print(f"File content:\n{content}")
